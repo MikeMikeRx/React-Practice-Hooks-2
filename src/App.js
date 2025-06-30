e8935d74d47d944e124de88deca16cb31b9836a7
MichalRais <Michal.Rais@iCloud.com>
2025-06-30 11:51:18 +0100

    useEffect, useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
 
-function App() {
+
+const App = () => {
+  const [value, setValue] = useState(0)
+  const [test, setTest] = useState(0)
+
+  useEffect ( () =>{
+    const button = document.querySelector(".btn")
+    if (value >=1) {
+      button.textContent = `Clicked ${value}x`
+    } else { 
+      button.textContent = "Click"
+    }
+    console.log("First-test")
+  },[value])
+  
+  
+  useEffect( ()=> {
+    document.title = `Test Title ${test}`
+    console.log("Second-test")
+  },[test])
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Test</h1>
+    <p>{value}</p>
+    <button className="btn" onClick={ ()=> setValue (value + 1)}>Click</button>
+    <button onClick={ ()=> setTest (test + 1)}>Title</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
